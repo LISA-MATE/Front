c14b5db284887f62850881e0d614011116d21bf3
sheepyis <113487989+sheepyis@users.noreply.github.com>
2023-07-07 23:08:49 +0900

    Delete static directory

--- static/javascript/login.js
+++
@@ -1,6 +0,0 @@
-const saveUser = document.getElementById("saveUser");
-const saveLogin = document.getElementById("saveLogin");
-
-saveUser.addEventListener("click", function () {
-    saveLogin.checked = !saveLogin.checked;
-});
